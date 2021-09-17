bac2677c0e1ed15c30a58773c0b8f990fda424a8
zwz <akazwz@pm.me>
2021-09-17 23:30:30 +0800

    init

--- App.js
+++ App.js
@@ -1,21 +1,112 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    let openImagePickerAsync = async() => {
+        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if ( permissionResult.granted === false ) {
+            alert("Permission to access camera roll is requires!");
+            return;
+        }
+
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+        console.log(pickerResult);
+
+        const {cancelled, uri} = pickerResult;
+        if ( cancelled === true ) {
+            return;
+        }
+
+        setSelectedImage({localUri: uri});
+    };
+
+    let openShareDialogAsync = async() => {
+        if ( !(await Sharing.isAvailableAsync()) ) {
+            alert(`Uh oh, sharing isn't available on your platform! `);
+            return;
+        }
+        await Sharing.shareAsync(selectedImage.localUri);
+    };
+
+    if ( selectedImage !== null ) {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: selectedImage.localUri}}
+                    style={styles.thumbnail}
+                />
+                <TouchableOpacity
+                    onPress={openShareDialogAsync}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>
+                        Share this photo
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{uri: "https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2677520025.webp"}}
+                style={styles.post}
+            />
+            <Text style={styles.instructions}>
+                To share a photo from your phone with a friend, just press the button below!
+            </Text>
+
+            <TouchableOpacity
+                onPress={openImagePickerAsync}
+                style={styles.button}
+            >
+                <Text style={styles.buttonText}>
+                    Pick a photo
+                </Text>
+
+            </TouchableOpacity>
+
+            <StatusBar style='auto'/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    post: {
+        width: 270,
+        height: 400,
+        marginBottom: 10,
+    },
+    instructions: {
+        color: '#888',
+        fontSize: 18,
+        marginHorizontal: 15,
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    },
+    thumbnail: {
+        width: 300,
+        height: 300,
+        resizeMode: "contain",
+    },
 });
