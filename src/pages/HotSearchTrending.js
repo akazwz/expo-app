d2bf1b21b292a3e4b95807e2fdfd761911998bfe
zwz <akazwz@pm.me>
2021-09-20 02:22:33 +0800

    势

--- src/pages/HotSearchTrending.js
+++ src/pages/HotSearchTrending.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, StatusBar, View } from 'react-native';
-import { Button, Text } from 'react-native-elements';
+import { TouchableOpacity, ScrollView, StyleSheet, StatusBar, View } from 'react-native';
+import { Button, Text, Input } from 'react-native-elements';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import RNEChartsPro from "react-native-echarts-pro";
 import moment from 'moment';
 import * as Linking from 'expo-linking';
@@ -14,6 +15,7 @@ const HotSearchTrending = () => {
     const defaultDataset = [['time', 'rank', 'hot']];
     const [hotSearchesDataset, setHotSearchesDataset] = useState(defaultDataset);
     const [showChart, setShowChart] = useState(false);
+    const [searchText, setSearchText] = useState();
     const getHotSearches = (cont, start, stop) => {
         GetHotSearchesByContent(cont, start, stop)
             .then((res) => {
@@ -46,9 +48,41 @@ const HotSearchTrending = () => {
     };
     return (
         <View>
-            <Button onPress={handleBtnPress} title='refresh'/>
-            {showChart ? <RankTrending source={hotSearchesDataset}/>: null}
-
+            <View style={{flexDirection: 'row'}}>
+                <Input
+                    leftIcon={
+                        <Ionicons
+                            name='ios-search'
+                            size={17}
+                            color='#8C8C8C'
+                        />
+                    }
+                    placeholder={'大家正在搜: 王一博被扛走了'}
+                    placeholderTextColor='#8C8C8C'
+                    leftIconContainerStyle={{
+                        marginLeft: '3%'
+                    }}
+                    inputContainerStyle={{
+                        borderWidth: 0,
+                        borderBottomWidth: 0,
+                        borderRadius: 9,
+                        height: 35,
+                        backgroundColor: '#D9D9D9'
+                    }}
+                    containerStyle={{
+                        height: 37,
+                        flex: 1,
+                    }}
+                    inputStyle={{
+                        textAlign: 'left',
+                        fontSize: 17,
+                    }}
+                />
+                <TouchableOpacity>
+                    <Text style={{fontSize: 17}}>取消</Text>
+                </TouchableOpacity>
+            </View>
+            {showChart ? <RankTrending source={hotSearchesDataset}/> : null}
         </View>
     );
 };
